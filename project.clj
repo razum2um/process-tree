a20de2981ec731696225c66f5846b7e3b6febeab
Vlad Bokov <razum2um@mail.ru>
2014-07-03 01:34:44 +0700

    next snapshot started

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject process-tree "0.1.0"
+(defproject process-tree "0.1.1-SNAPSHOT"
   :description "Manage your dependent processes in declarative way"
   :url "http://github.com/razum2um/process-tree"
   :license {:name "Eclipse Public License"
